cd0c103c5f68053969823731cd337c384bdcae4e
Melisa <melisahrnn@gmail.com>
2023-04-26 20:02:11 -0400

    JavaScript File added

---
+++ app.js
@@ -0,0 +1,62 @@
+function generateChart(){
+    //Fetch data.json
+    fetch('/data.json')
+        .then((res)=> res.json())
+        .then((data) => {
+            const info= {
+                //Days
+                labels: data.map((chart)=>chart.day), 
+                datasets:[
+                {
+                    //Amount
+                    data: data.map((chart) =>chart.amount),
+                    backgroundColor:["hsl(10, 79%, 65%)"],    
+                    borderRadius: 5,
+                    hoverBackgroundColor:"hsl(186, 34%, 60%)",
+                }
+                ]
+            }
+            //$ sign
+            const titleTooltip=(e)=>`$${e[0].formattedValue}`;
+            const labelTooltip = (e) => "";
+            const options = {
+                scales:{
+                    y: {
+                        ticks:{
+                            display:false
+                        },
+                    grid: {
+                        display: false,
+                        drawTicks:false,
+                        drawBorder:false
+                    },     
+                    },
+                    x: {
+                        grid: {
+                            display: false,
+                            drawBorder:false,
+                        }
+                    }
+                },
+                plugins: {
+                    legend: {display:false},
+                    tooltip:{
+                        yAlign:"bottom",
+                        displayColors: false,
+                        callbacks:{
+                            title: titleTooltip,
+                            label:labelTooltip,
+                        }
+                    }
+                }
+            }
+            const config ={
+                //Bar Chart
+                type:"bar",
+                data:info,
+                options
+            }
+            const myChart = new Chart(document.getElementById('myChart'), config)
+        })
+}
+generateChart();
