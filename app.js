d13efd483b177b7f9f4e03d772e3f0d1d6fba9e7
Melisa <melisahrnn@gmail.com>
2023-04-27 14:26:12 -0400

    JavaScript Updated

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 function generateChart(){
     //Fetch data.json
-    fetch('./data.json')
+    fetch(`./data.json`)
         .then((res)=> res.json())
         .then((data) => {
             const info= {
